b14751870423c752e0dad0d709790f2fbe02a1ec
liuponghei <liuponghei@users.noreply.github.com>
2018-07-11 02:40:33 +0800

    Update common.mk

--- common.mk
+++ common.mk
@@ -88,12 +88,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.text_large_cache_height=1024
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapgrowthlimit=512m \
     dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapsize=768m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
+    dalvik.vm.heapmaxfree=64m
 
 # Display
 PRODUCT_PACKAGES += \
